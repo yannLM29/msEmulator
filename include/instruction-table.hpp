43cfbdb0d09e69e6ed5e94fc3fb44deb51a17de5
yannLM29 <yann.lemasson33@gmail.com>
2023-11-04 19:34:54 +0100

    decomposed_addr_mode_version

---
+++ include/instruction-table.hpp
@@ -0,0 +1,13 @@
+// #pragma once
+
+// #include <string>
+// #include <functional>
+// #include "register-types.hpp"
+
+// struct InstructionTable
+// {
+//     std::string instruction_name;
+//     int opcode;
+//     std::function address;
+//     s
+// };
--- include/z80.hpp
+++ include/z80.hpp
@@ -11,7 +11,7 @@ class z80
 {
 
 public:     // a changer
-    Register2x8b AF, BC, DE, HL;
+    Register2x8b BC, DE, HL, AF;
     Register2x8b AFp, BCp, DEp, HLp;
 
     Register16b IX, IY, SP;
@@ -19,6 +19,9 @@ public:     // a changer
     Register8b I, R;
 
     Register16b PC;
+    
+    uint16_t to_read;
+    uint16_t to_write;
 
 private:
     Ram &cpu_ram_;
@@ -28,8 +31,92 @@ public:
     // ~z80();
 
     void reset() noexcept;
+
+
+    // -------- DataFrom --------
+    // ---- Registers ----
+    void DataFromR_A(std::vector<uint8_t>opcode)
+    {
+        to_read = (uint16_t)AF.r8.msb;
+    }
+    void DataFromR_B()
+    {
+        to_read = (uint16_t)BC.r8.msb;
+    }
+    void DataFromR_C()
+    {
+        to_read = (uint16_t)BC.r8.lsb;
+    }
+    void DataFromR_D()
+    {
+        to_read = (uint16_t)DE.r8.msb;
+    }
+    void DataFromR_E()
+    {
+        to_read = (uint16_t)DE.r8.lsb;
+    }
+    void DataFromR_H()
+    {
+        to_read = (uint16_t)HL.r8.msb;
+    }
+    void DataFromR_L()
+    {
+        to_read = (uint16_t)HL.r8.lsb;
+    }
+
+    // ---- Adresses ----
+    void DataFromHLaddr()
+    {
+        to_read = (uint16_t)cpu_ram_.read(HL.r16);
+    }
+
+
+    // ---------------- DataTo ----------------
+    // ---- Registers ----
+    void DataToR_A()
+    {
+        AF.r8.msb = uint8_t(to_write);
+    }
+
+    void DataToR_B()
+    {
+        BC.r8.msb = uint8_t(to_write);
+    }
+    void DataToR_C()
+    {
+        BC.r8.lsb = uint8_t(to_write);
+    }
+
+    void DataToR_D()
+    {
+        DE.r8.msb = uint8_t(to_write);
+    }
+    void DataToR_E()
+    {
+        DE.r8.lsb = uint8_t(to_write);
+    }
+
+    void DataToR_H()
+    {
+        HL.r8.msb = uint8_t(to_write);
+    }
+    void DataToR_L()
+    {
+        HL.r8.lsb = uint8_t(to_write);
+    }
+    
+    void LD()
+    {
+        to_write = to_read;
+
+    }
 };
 
+template<typename reg_type>
+void LD(reg_type from, reg_type &to)
+{
+    to = from;
+}
 
 
 
